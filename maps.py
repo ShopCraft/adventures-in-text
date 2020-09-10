ea0f29cc240d2f58379c57c089c12ac8aad08069
Murriel <murriel@murrielgrace.com>
2020-09-09 20:29:33 -0700

    Revert "Revert "updated gitignore for workspaces""
    This reverts commit e3db62a2ed2ba0679f2010ac505793f0840935d7.

--- maps.py
+++ maps.py
@@ -1,3 +1,17 @@
 '''
 Map Game Data. Descriptions of the locations in the game world. 
 '''
+import requests, json
+
+metroapi="https://api.metro.net/agencies/lametro-rail/"
+trains="/agencies/lametro-rail/"
+
+print(metroapi)
+
+response = requests.get(metroapi)
+json_response = response.json()
+
+print(response)
+print(json_response)
+
+print(json.dumps(json_response, indent=1))
